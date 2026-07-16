0d68eb96277ecc8d8b5a10c9695737d65779e758
Akhil <chamalaakhilreddy@gmail.com>
2026-07-16 23:52:26 +0530

    feat:gave ultron vision

---
+++ vision/analyzer.py
@@ -0,0 +1,4 @@
+answer = analyzer.analyze(
+    image_path="assets/image.jpg",
+    prompt="What do you see?"
+)
---
+++ vision/camera.py
@@ -0,0 +1,30 @@
+import cv2
+import time
+
+
+class Camera:
+    def __init__(self,camera_index:int=0):
+        self.camera_index=camera_index
+
+    def capture_frame(self):
+        cap=cv2.VideoCapture(self.camera_index)
+
+        if not cap.isOpened():
+            raise RuntimeError("Unable to open webcam")
+
+        time.sleep(0.5)
+
+        success,frame=cap.read()
+
+        cap.release()
+        if not success:
+            raise RuntimeError("Failed to Capture image")
+
+        return frame
+
+    def save_frame(self,filepath:str):
+        frame =self.capture_frame()
+        cv2.imwrite(filepath,frame)
+
+        return filepath
+
